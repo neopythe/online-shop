5300e103492b52213ecd786b33404ca309e50e6c
Evan Bay <neopythe@protonmail.ch>
2022-06-05 10:10:57 -0600

    add customer templates

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const path = require('path')
 const express = require('express')
+const ejsMate = require('ejs-mate')
 
 const authRoutes = require('./routes/auth.routes')
 
 const app = express()
 const port = 3000
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(authRoutes)
 
